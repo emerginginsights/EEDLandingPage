955a2a0bda04b51f8372ed90ac70cf443676af07
emerginginsights <65678311+emerginginsights@users.noreply.github.com>
2020-06-04 16:07:38 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
-let mainInput = document.querySelector('.normal__width');
-let mainInputMin = document.querySelector('.width__500');
+let mainInput = document.querySelector('#main__input');
+let mainInputMin = document.querySelector('#main__input-min');
 let titleBlock = document.querySelector('.title__block');
 let imgBlock = document.querySelector('.img__block');
 let wrap = document.querySelector('.wrap');
@@ -9,20 +9,68 @@ let mainContent = document.querySelector('.main__content');
 let countriesBlock = document.querySelector('.countries__block');
 let tipsBlock = document.querySelector('.tips__block');
 let tipsBlockMini = document.querySelector('.tips__block__500');
+console.log(mainInput.value);
 
-mainInput.addEventListener('click', () => {
-	countriesBlock.classList.toggle('invisible');
-	tipsBlock.classList.toggle('invisible');
+mainInput.addEventListener('focus', () => {
+	countriesBlock.classList.add('invisible');
+})
+mainInput.addEventListener('blur', () => {
+	countriesBlock.classList.remove('invisible');
+	tipsBlock.classList.add('invisible');
 })
 
+mainInputMin.addEventListener('focusin', () => {
+	titleBlock.classList.add('invisible');
+	imgBlock.classList.add('invisible');
+	wrap.classList.add('max-height');
+	mainContent.classList.add('section__mt');
+});
+mainInputMin.addEventListener('focusout', () => {
+	titleBlock.classList.remove('invisible');
+	imgBlock.classList.remove('invisible');
+	wrap.classList.remove('max-height');
+	mainContent.classList.remove('section__mt');
+	tipsBlockMini.classList.add('invisible');
+})
+
+function filterFunction() {
+	let filter = mainInput.value.toUpperCase();
+	let p = tipsBlock.getElementsByTagName("p");
+	tipsBlock.classList.remove('invisible');
+    for (let i = 0; i < p.length; i++) {
+    	let txtValue = p[i].innerHTML;
+    	if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      		p[i].classList.remove('invisible');
+  		}else {
+      		p[i].classList.add('invisible');
+    	}
+  	}
+  	let pArray = [...p];
+  	pArray.forEach((item) => {
+  		item.addEventListener('mouseover', () => {
+  			mainInput.value = item.innerHTML;
+  		})
+  	})
+}
 
 
-if (window.matchMedia("screen and (min-width: 500px)")){
-	mainInputMin.addEventListener('click', () => {
-		titleBlock.classList.toggle('invisible');
-		imgBlock.classList.toggle('invisible');
-		wrap.classList.toggle('max-height');
-		mainContent.classList.toggle('section__mt');
-		tipsBlockMini.classList.toggle('invisible');
-	})
+function filterFunctionMini() {
+	let filter = mainInputMin.value.toUpperCase();
+	let p = tipsBlockMini.getElementsByTagName("p");
+	tipsBlockMini.classList.remove('invisible');
+    for (let i = 0; i < p.length; i++) {
+    	let txtValue = p[i].innerHTML;
+    	if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      		p[i].classList.remove('invisible');
+  		}else {
+      		p[i].classList.add('invisible');
+    	}
+  	}
+  	let pArray = [...p];
+  	console.log(pArray);
+  	pArray.forEach((item) => {
+  		item.addEventListener('mouseover', () => {
+  			mainInputMin.value = item.innerHTML;
+  		})
+  	})
 }
